4c673468f4590d3236d1f27b108c93582dbfd2cb
Ryan Schaffer <rys686@mail.usask.ca>
2024-09-06 00:40:33 -0600

    added loadDDS from tutorials

--- lib/libdemo.hpp
+++ lib/libdemo.hpp
@@ -7,6 +7,7 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
 
 // from common/texture.cpp
 GLuint loadBMP_custom(const char * imagepath);
+GLuint loadDDS(const char * imagepath);
 
 
 
