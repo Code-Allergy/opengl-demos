09e18211b7ba81e6c907931fdb2f2b59898bdb3c
Ryan Schaffer <rys686@mail.usask.ca>
2024-09-05 22:59:36 -0600

    added simple bmp loader from tutorials

--- lib/libdemo.hpp
+++ lib/libdemo.hpp
@@ -5,6 +5,9 @@
 
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
 
+// from common/texture.cpp
+GLuint loadBMP_custom(const char * imagepath);
+
 
 
 #endif // LIBDEMO_H
