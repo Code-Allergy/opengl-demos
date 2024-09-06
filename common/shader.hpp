9b405b1ec56d2c1c1b231a26ed9c060228f91029
Ryan Schaffer <rys686@mail.usask.ca>
2024-09-05 19:57:33 -0600

    added demo 1 - triangle

---
+++ common/shader.hpp
@@ -0,0 +1,7 @@
+// Shader loader imported directly from the OpenGL tutorial at http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif
